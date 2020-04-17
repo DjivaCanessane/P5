280ebcb4a40cc230b1674a76546da39a51cf8057
Djiva Canessane <dcanessane@icloud.com>
2020-04-17 03:06:57 +0200

    Created Model Arithmetics.swift and ArithmeticsTestCase

---
+++ CountOnMe/Models/Arithmetics.swift
@@ -0,0 +1,13 @@
+//
+//  Arithmetics.swift
+//  CountOnMe
+//
+//  Created by Djiva Canessane on 17/04/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Arithmetics {
+    
+}
--- CountOnMeTests/ArithmeticsTestCase.swift
+++ CountOnMeTests/ArithmeticsTestCase.swift
@@ -1,29 +1,29 @@
 //
-//  SimpleCalcTests.swift
-//  SimpleCalcTests
+//  ArithmeticsTestCase.swift
+//  CountOnMeTests
 //
-//  Created by Vincent Saluzzo on 29/03/2019.
-//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//  Created by Djiva Canessane on 17/04/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
 //
 
 import XCTest
 
-class SimpleCalcTests: XCTestCase {
+class ArithmeticsTestCase: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
